eebe250a3c60e14916defa339046f35ff2429151
JodieFlacko <96775468+JodieFlacko@users.noreply.github.com>
2025-10-19 13:26:24 +0200

    style: page look and structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,11 @@
 import "./styles/style.css";
 import "./styles/css-reset-by-Josh-Cameau.css";
+import {createNote, getNote, getNotes, removeNote, removeProject} from "../handleNotes";
+import { displayTaskElements, displayProjects } from "../displayNotes";
+import { initEventListeners } from "../handleEventListeners";
+
+displayTaskElements();
+displayProjects();
+initEventListeners();
+removeProject("Personal Project");
+displayProjects();
